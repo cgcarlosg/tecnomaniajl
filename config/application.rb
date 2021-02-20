020afd3d52c71e1b604764b21e3e1fea1c10ba56
carlos <cgcarlosg@hotmail.com>
2021-02-20 17:21:50 -0500

    initial commit

---
+++ config/application.rb
@@ -0,0 +1,34 @@
+require_relative "boot"
+
+require "rails/all"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Tecnomaniajl
+  class Application < Rails::Application
+
+    config.to_prepare do
+      # Load application's model / class decorators
+      Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
+        Rails.configuration.cache_classes ? require(c) : load(c)
+      end
+
+      # Load application's view overrides
+      Dir.glob(File.join(File.dirname(__FILE__), "../app/overrides/*.rb")) do |c|
+        Rails.configuration.cache_classes ? require(c) : load(c)
+      end
+    end
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.1
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+  end
+end
---
+++ db/migrate/20210220213992_add_social_to_spree_stores.spree.rb
@@ -0,0 +1,11 @@
+# This migration comes from spree (originally 20200102141311)
+class AddSocialToSpreeStores < ActiveRecord::Migration[6.0]
+  def change
+    add_column :spree_stores, :facebook, :string
+    add_column :spree_stores, :twitter, :string
+    add_column :spree_stores, :instagram, :string
+
+    # Fix cache issue #10381
+    Rails.cache.delete('default_store')
+  end
+end
